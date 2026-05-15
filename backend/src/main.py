680bbb9558af14a5125da13ca084426aafedecf9
Doonminus2 <piezapiezakok@gmail.com>
2026-05-15 17:41:41 +0700

    feat: add endpoint and generate_schama_endpoint (mock)

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI 
+from schemas import PromptRequest , SchemaResponse
 
 app = FastAPI()
 
 @app.get("/")
 def home(): 
-    return {"message": "Promt to Schema API is running!"}
+    return {"message": "Promp to Schema API is running!"}
+
+@app.post("/api/generate", response_model=SchemaResponse )
+async def generate_schema_endpoint(request : PromptRequest):
+    user_text = request.requirement
+    
+@app.get("/health")
+def health_check():
+    return {"status" : "OK"}
+
+
+    
+    
 
 
